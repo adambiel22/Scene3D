3bb2ce963b1b4e266086c424023c748ba4b062c4
adambiel22 <51017764+adambiel22@users.noreply.github.com>
2022-01-30 17:50:44 +0100

    Projection class completed

--- 3DAdamBielecki/3DScene/Projection.cs
+++ 3DAdamBielecki/3DScene/Projection.cs
@@ -1,17 +1,54 @@
 ﻿using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 
 namespace _3DAdamBielecki._3DScene
 {
     public class Projection
     {
-        public Triangle Project(Triangle triangle)
+        private Matrix projectionMatrix;
+        private double fieldOfView;
+        private double farPlaneDistance;
+        private double aspectRatio;
+        private double nearPlaneDistance;
+        public double FieldOfView { get => fieldOfView; set { fieldOfView = value; UpdateProjectionMatrix(); } }
+        public double FarPlaneDistance { get => farPlaneDistance; set { farPlaneDistance = value; UpdateProjectionMatrix(); } }
+        public double AspectRatio { get => aspectRatio; set { aspectRatio = value; UpdateProjectionMatrix(); } }
+        public double NearPlaneDistance { get => nearPlaneDistance; set { nearPlaneDistance = value; UpdateProjectionMatrix(); } }
+        public int ScreenWidth { get; set; }
+        public int ScreenHeight { get; set; }
+
+        public Projection(double fieldOfView, double farPlaneDistance, double aspectRatio, double nearPlaneDistance)
         {
-            return triangle;
+            this.fieldOfView = fieldOfView;
+            this.farPlaneDistance = farPlaneDistance;
+            this.aspectRatio = aspectRatio;
+            this.nearPlaneDistance = nearPlaneDistance;
+            projectionMatrix = DenseMatrix.Create(4, 4, 0);
+            UpdateProjectionMatrix();
         }
 
         public Vector Project(Vector vector)
         {
-            return vector;
+            Vector projectedVector = (Vector)(projectionMatrix * vector);
+            projectedVector[0] /= projectedVector[3];
+            projectedVector[1] /= projectedVector[3];
+            projectedVector[2] /= projectedVector[3];
+            projectedVector[3] = 1;
+            return projectedVector;
+        }
+        private void UpdateProjectionMatrix()
+        {
+            double focalLength = 1.0 / Math.Tan(FieldOfView / 2);
+
+            projectionMatrix[0, 0] = focalLength;
+            projectionMatrix[1, 1] = focalLength / aspectRatio;
+            projectionMatrix[2, 2] =
+                (farPlaneDistance + nearPlaneDistance)
+                / (nearPlaneDistance - farPlaneDistance);
+            projectionMatrix[2, 3] =
+                2 * nearPlaneDistance * nearPlaneDistance
+                / (nearPlaneDistance - farPlaneDistance);
+            projectionMatrix[3, 2] = -1;
         }
     }
 }
--- 3DAdamBielecki/3DScene/TriangleProjector.cs
+++ 3DAdamBielecki/3DScene/TriangleProjector.cs
@@ -9,9 +9,9 @@ namespace _3DAdamBielecki._3DScene
 {
     public class TriangleProjector
     {
+        public Transformation Transformation { get; set; }
         public Camera Camera { get; set; }
         public Projection Projection { get; set; }
-        public Transformation Transformation { get; set; }
         
         Triangle ProjectTriangle(Triangle triangle)
         {
@@ -28,9 +28,13 @@ namespace _3DAdamBielecki._3DScene
                     Projection.Project(
                         Camera.LookAt(
                             Transformation.Transform(vectors[i])));
+                vectors[i][0] /= vectors[i][3];
+                vectors[i][1] /= vectors[i][3];
+                vectors[i][2] /= vectors[i][3];
+                vectors[i][3] = 1;
                 //jeszcze potrzebna jest transformacja wektora normalnego
             }
-            //a tutaj jeszcze potrzebne jest normalizowanie i rozciągnięcie? no chyba tak.
+            //a tutaj jeszcze potrzebne jest rozciągnięcie? no chyba tak.
             Triangle projectedTriangle = new Triangle()
         }
     }
